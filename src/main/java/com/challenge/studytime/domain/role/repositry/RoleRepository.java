70e7948a6e17edfc99f7f7c1c4b9479a1a8bf2d5
KMGeon <pos04167@kakao.com>
2023-04-07 14:05:50 +0900

    ✨feat: 권한 Repository 추가

--- src/main/java/com/challenge/studytime/domain/role/repositry/RoleRepository.java
+++ src/main/java/com/challenge/studytime/domain/role/repositry/RoleRepository.java
@@ -3,5 +3,8 @@ package com.challenge.studytime.domain.role.repositry;
 import com.challenge.studytime.domain.role.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepositry extends JpaRepository<Role,Long> {
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Optional<Role> findByName(String name);
 }
