676d6a93a295a908a5bc0ccf4c4f0e09aa8e03e8
KMGeon <pos04167@kakao.com>
2023-04-09 18:55:50 +0900

    ♻️refactor: 연관 관계 N+1 문제 FetchJoin으로 리펙토링

--- src/main/java/com/challenge/studytime/domain/study/repository/StudyRepository.java
+++ src/main/java/com/challenge/studytime/domain/study/repository/StudyRepository.java
@@ -2,6 +2,12 @@ package com.challenge.studytime.domain.study.repository;
 
 import com.challenge.studytime.domain.study.entity.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface StudyRepository extends JpaRepository<Study, Long>,StudyRepositoryCustom {
+import java.util.Optional;
+
+public interface StudyRepository extends JpaRepository<Study, Long>, StudyRepositoryCustom {
+    @Query("select s from Study s where s.id=:id AND s.deleteStudy=false ")
+    Optional<Study> findByIdAndDeleteStudyFalse(@Param("id") Long id);
 }
