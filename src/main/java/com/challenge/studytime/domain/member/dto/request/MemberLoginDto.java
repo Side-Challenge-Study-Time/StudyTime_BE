0fd71fb5a40a39119300917aee023a05a5d7a611
KMGeon <pos04167@kakao.com>
2023-04-14 19:12:39 +0900

    ♻️refactor: Update IfLogin Code Convention

--- src/main/java/com/challenge/studytime/domain/member/dto/request/MemberLoginDto.java
+++ src/main/java/com/challenge/studytime/domain/member/dto/request/MemberLoginDto.java
@@ -13,11 +13,11 @@ import javax.validation.constraints.Pattern;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberLoginDto {
-    @NotEmpty
-    @Pattern(regexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")
+    @NotEmpty(message = "이메일은 필수적으로 입력하세요.")
+    @Pattern(regexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", message = "이메일 양식을 지켜주세요")
     private String email;
 
-    @NotEmpty
+    @NotEmpty(message = "비밀번호를 입력하세요.")
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,20}$")
     private String password;
 
