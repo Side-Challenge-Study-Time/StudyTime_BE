5679660bc457320cc470d964792dd322af6332bd
KMGeon <pos04167@kakao.com>
2023-04-17 21:06:59 +0900

    ♻️refactor: 테스트 코드 추가를 위한 빌드패턴 추가

--- src/main/java/com/challenge/studytime/domain/refreshToken/dto/request/RefreshTokenDto.java
+++ src/main/java/com/challenge/studytime/domain/refreshToken/dto/request/RefreshTokenDto.java
@@ -1,10 +1,15 @@
 package com.challenge.studytime.domain.refreshToken.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
-
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RefreshTokenDto {
     @NotEmpty(message = "refresh token을 입력하세요.")
     String refreshToken;
