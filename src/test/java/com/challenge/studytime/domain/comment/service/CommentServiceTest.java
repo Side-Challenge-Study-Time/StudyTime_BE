82787cab64765fd8bc348e455241726afce22fbe
KMGeon <pos04167@kakao.com>
2023-04-18 17:04:50 +0900

    ♻️refactor: 테스트 코드 리펙토링

--- src/test/java/com/challenge/studytime/domain/comment/service/CommentServiceTest.java
+++ src/test/java/com/challenge/studytime/domain/comment/service/CommentServiceTest.java
@@ -1,5 +1,6 @@
 package com.challenge.studytime.domain.comment.service;
 
+import com.challenge.studytime.domain.comment.dto.response.CommentDto;
 import com.challenge.studytime.enums.TestValidEnum;
 import com.challenge.studytime.domain.comment.repository.CommentRepository;
 import com.challenge.studytime.domain.member.entity.Member;
@@ -12,6 +13,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -30,11 +33,11 @@ class CommentServiceTest {
     @Autowired
     private StudyRepository studyRepository;
 
-    private static final String TITLE ="제목";
-    private static final String CONTENT ="내용";
+    private static final String TITLE = "제목";
+    private static final String CONTENT = "내용";
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         Member member = Member.builder()
                 .email(TestValidEnum.VALID_EMAIL.getMessage())
                 .name(TestValidEnum.VALID_NAME.getMessage())
@@ -44,7 +47,7 @@ class CommentServiceTest {
 
         memberRepository.save(member);
 
-        Study study =Study.builder()
+        Study study = Study.builder()
                 .title(TITLE)
                 .content(CONTENT)
                 .membersCount(100)
@@ -53,18 +56,59 @@ class CommentServiceTest {
     }
 
     @Test
-    @DisplayName("댓글 생성 테스트 로직")
-    public void commentWithValid() throws Exception{
+    @DisplayName("부모 댓글 생성 테스트")
+    public void createParentCommentTest() {
         //given
         LoginUserDto userDto = LoginUserDto.builder()
                 .memberId(1L)
                 .build();
-
         Long studyId = 1L;
+
         //when
-        commentService.createParentComment(CONTENT,userDto,studyId);
-        //Then
-        assertThat(commentRepository.count()).isNotNull();
-        assertThat(commentRepository.findById(1L).get().getContent()).isEqualTo(CONTENT);
+        CommentDto commentDto = commentService.createParentComment(CONTENT, userDto, studyId);
+
+        //then
+        assertThat(commentDto.getId()).isNotNull();
+        assertThat(commentDto.getContent()).isEqualTo(CONTENT);
+    }
+
+    @Test
+    @DisplayName("부모 댓글에 대한 답글 생성 테스트")
+    public void createReplyCommentTest() {
+        //given
+        LoginUserDto userDto = LoginUserDto.builder()
+                .memberId(1L)
+                .build();
+        Long studyId = 1L;
+
+        CommentDto parentCommentDto = commentService.createParentComment(CONTENT, userDto, studyId);
+
+        //when
+        CommentDto replyCommentDto = commentService.createReplyComment(parentCommentDto.getId(), CONTENT, userDto);
+
+        //then
+        assertThat(replyCommentDto.getId()).isNotNull();
+        assertThat(replyCommentDto.getContent()).isEqualTo(CONTENT);
+        assertThat(replyCommentDto.getParentId()).isEqualTo(parentCommentDto.getId());
+    }
+
+    @Test
+    @DisplayName("모든 댓글과 답글 검색 테스트")
+    public void fullSearchWithCommentTest() {
+        //given
+        LoginUserDto userDto = LoginUserDto.builder()
+                .memberId(1L)
+                .build();
+        Long studyId = 1L;
+
+        CommentDto parentCommentDto = commentService.createParentComment(CONTENT, userDto, studyId);
+
+        CommentDto replyCommentDto = commentService.createReplyComment(parentCommentDto.getId(), CONTENT, userDto);
+
+        //when
+        List<CommentDto> commentDtoList = commentService.fullSrchWithComment();
+
+        //then
+        assertThat(commentDtoList).hasSize(3);
     }
 }
