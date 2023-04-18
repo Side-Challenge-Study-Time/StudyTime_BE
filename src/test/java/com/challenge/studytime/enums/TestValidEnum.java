def5204134f2f3c207c48219e7e4f1240816a725
KMGeon <pos04167@kakao.com>
2023-04-18 16:55:09 +0900

    ♻️refactor: VALID에 맞게 값 수정

--- src/test/java/com/challenge/studytime/enums/TestValidEnum.java
+++ src/test/java/com/challenge/studytime/enums/TestValidEnum.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.DisplayName;
 @DisplayName("테스트 고정적인 이름을 ENUM으로 명확하게 표현")
 public enum TestValidEnum {
 
-    VALID_EMAIL("test@email.com"),
-    VALID_PASSWORD("1234"),
+    VALID_EMAIL("test1234@email.com"),
+    VALID_PASSWORD("Aa1!bbccddeeff"),
     VALID_NAME("김무건"),
     VALID_BIRTHDAY("2000-01-01");
 
